2e5699a8a268500ec0b2589674372dd7e6993d7c
Shivam Dwivedi <shivamd20@gmail.com>
2026-02-07 04:41:50 +0530

    ljl

--- src/components/system-shots/useReelsFeed.ts
+++ src/components/system-shots/useReelsFeed.ts
@@ -184,7 +184,7 @@ export function useReelsFeed(options: UseReelsFeedOptions = {}): UseReelsFeedRet
   // Debug log for current reels
   useEffect(() => {
     if (allReels.length > 0) {
-      console.log(`[useReelsFeed] Current Reels List (${allReels.length}):`, allReels.map(r => `${r.id} (${r.conceptId})`))
+      // console.log(`[useReelsFeed] Current Reels List (${allReels.length}):`, allReels.map(r => `${r.id} (${r.conceptId})`))
     }
   }, [allReels])
 
